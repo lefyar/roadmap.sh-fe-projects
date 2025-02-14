9f34206d4dd536ad1c48cee68f3fac8a91d2044d
Rey <120945128+reyfaldy@users.noreply.github.com>
2025-02-14 14:52:41 +0700

    Update vite.config.js

--- 15-flash-cards/vite.config.js
+++ 15-flash-cards/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/roadmap.sh-fe-projects/15-Flash%20Cards/flashcards-app/',
+  base: '/roadmap.sh-fe-projects/15-flash-cards/',
   plugins: [react()],
 })

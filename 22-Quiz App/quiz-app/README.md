5999c5f6997f8ca8f03ef5123aef53bab8e02df5
Rey <120945128+reyfaldy@users.noreply.github.com>
2025-02-27 15:08:06 +0700

    Formatting code

--- 22-Quiz App/quiz-app/README.md
+++ 22-Quiz App/quiz-app/README.md
@@ -1,4 +1,8 @@
-# React + TypeScript + Vite
+# Quiz App (In Progress)
+
+A quiz app where it will present users with a series of multiple-choice questions and after each question, the app will provide immediate feedback on whether the selected answer is correct or incorrect.
+
+# React + TypeScript + Vite README
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
@@ -18,11 +22,11 @@ export default tseslint.config({
   languageOptions: {
     // other options...
     parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
       tsconfigRootDir: import.meta.dirname,
     },
   },
-})
+});
 ```
 
 - Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
@@ -31,11 +35,11 @@ export default tseslint.config({
 
 ```js
 // eslint.config.js
-import react from 'eslint-plugin-react'
+import react from "eslint-plugin-react";
 
 export default tseslint.config({
   // Set the react version
-  settings: { react: { version: '18.3' } },
+  settings: { react: { version: "18.3" } },
   plugins: {
     // Add the react plugin
     react,
@@ -44,7 +48,7 @@ export default tseslint.config({
     // other rules...
     // Enable its recommended rules
     ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
+    ...react.configs["jsx-runtime"].rules,
   },
-})
+});
 ```
--- 22-Quiz App/quiz-app/src/data/quizzes.ts
+++ 22-Quiz App/quiz-app/src/data/quizzes.ts
@@ -1,26 +1,32 @@
 export const Quizzes = [
-    {
-        id: 1,
-        question: "When did Indonesia become independent?",
-        options: ["August 1945", "September 1950", "December 1937", "January 1945"],
-        answer: "August 1945",
-    },
-    {
-        id: 2,
-        question: "Who is the 8th president of Indonesia?",
-        options: ["Soekarno", "Suharto", "Joko Widodo", "BJ Habibie", "Prabowo Subianto"],
-        answer: "Prabowo Subianto",
-    },
-    {
-        id: 3,
-        question: "Where is Indonesia located?",
-        options: ["South America", "South Asia", "Middle East", "South East Asia"],
-        answer: "South East Asia",
-    },
-    {
-        id: 4,
-        question: "What is the capital city of Indonesia?",
-        options: ["Bali", "Jakarta", "Yogyakarta", "Bandung"],
-        answer: "Jakarta",
-    }
-]
+  {
+    id: 1,
+    question: "When did Indonesia become independent?",
+    options: ["August 1945", "September 1950", "December 1937", "January 1945"],
+    answer: "August 1945",
+  },
+  {
+    id: 2,
+    question: "Who is the 8th president of Indonesia?",
+    options: [
+      "Soekarno",
+      "Suharto",
+      "Joko Widodo",
+      "BJ Habibie",
+      "Prabowo Subianto",
+    ],
+    answer: "Prabowo Subianto",
+  },
+  {
+    id: 3,
+    question: "Where is Indonesia located?",
+    options: ["South America", "South Asia", "Middle East", "South East Asia"],
+    answer: "South East Asia",
+  },
+  {
+    id: 4,
+    question: "What is the capital city of Indonesia?",
+    options: ["Bali", "Jakarta", "Yogyakarta", "Bandung"],
+    answer: "Jakarta",
+  },
+];

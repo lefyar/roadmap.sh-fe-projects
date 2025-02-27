b3eaf7f0c200b91fe01ccee8269d757b4a5be6ab
Rey <120945128+reyfaldy@users.noreply.github.com>
2025-02-27 22:34:03 +0700

    Update Styling

--- 03-Personal Portfolio/script.js
+++ 03-Personal Portfolio/script.js
@@ -1,21 +1,24 @@
-const theme = localStorage.getItem('theme');
+const theme = localStorage.getItem("theme");
 
 if (theme) {
-    document.documentElement.setAttribute('theme', theme);
-} else {    
-    document.documentElement.setAttribute('theme', 'dark');
+  document.documentElement.setAttribute("theme", theme);
+} else {
+  document.documentElement.setAttribute("theme", "dark");
 }
 
 function toggleDarkMode() {
-    const moonIcon = document.getElementById('moon_icon');
-    const sunIcon = document.getElementById('sun_icon');
-    const theme = document.documentElement.getAttribute('theme');
-    document.documentElement.setAttribute('theme', theme === 'dark' ? 'light' : 'dark');
-    if (theme === 'dark') {
-        moonIcon.style.display = 'none';
-        sunIcon.style.display = 'block';
-    } else {    
-        moonIcon.style.display = 'block';
-        sunIcon.style.display = 'none';
-    }
-}  
+  const moonIcon = document.getElementById("moon_icon");
+  const sunIcon = document.getElementById("sun_icon");
+  const theme = document.documentElement.getAttribute("theme");
+  document.documentElement.setAttribute(
+    "theme",
+    theme === "dark" ? "light" : "dark"
+  );
+  if (theme === "dark") {
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "block";
+  } else {
+    moonIcon.style.display = "block";
+    sunIcon.style.display = "none";
+  }
+}
